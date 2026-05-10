f0d2c1391be30022791e5a80f2020b1801ac8e21
LLUUZZQQ <lzq13328903829@163.com>
2026-05-11 03:33:21 +0800

    chore: worker deployment config for railway

---
+++ src/workers/index.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import "./image-worker";
+import "./video-worker";
+import "./swap-worker";
+import "./suggestion-worker";
+import "./trend-worker";
+
+console.log("All workers started");
