f32f03e2289aacac1ddd4f18d1a01196b5800bcb
LLUUZZQQ <lzq13328903829@163.com>
2026-05-12 08:14:06 +0800

    feat: add bg-replace queue

--- src/lib/queue.ts
+++ src/lib/queue.ts
@@ -6,3 +6,4 @@ export const imageQueue = new Queue("image-queue", { connection: redis });
 export const videoQueue = new Queue("video-queue", { connection: redis });
 export const suggestQueue = new Queue("suggest-queue", { connection: redis });
 export const swapQueue = new Queue("swap-queue", { connection: redis });
+export const bgReplaceQueue = new Queue("bg-replace-queue", { connection: redis });
